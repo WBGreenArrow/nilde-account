bf28007375e1668e632cb1ffad22f9199b246033
wellison E Brito <wellysone.brito@gmail.com>
2023-03-20 11:54:58 -0300

    feat: added background styles to home page

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+
+import './styles.scss'
+
 import { TransitionTypeCard } from '../../components/TransitionTypeCard'
 
 export const Home = () => {
   return (
-    <div>
-      <TransitionTypeCard label="Entrada" transitionType="deposit" />
+    <div className="homeContainer">
+      <div className="homeBackgroundHeader"></div>
+      <div className="homeBackgroundBody"></div>
+      <div className="homeContentWrapper">
+        <div className="homeContent"></div>
+      </div>
+      {/* <TransitionTypeCard label="Entrada" transitionType="deposit" /> */}
     </div>
   )
 }
