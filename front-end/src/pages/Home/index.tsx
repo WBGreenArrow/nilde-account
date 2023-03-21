3b10fbc3381418847ff86bbfdb22c1a8e7fd700c
wellison E Brito <wellysone.brito@gmail.com>
2023-03-21 11:14:40 -0300

    feat: added CardBalance component to Home page

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import './styles.scss'
 
 import { TransitionTypeCard } from '../../components/TransitionTypeCard'
 import { Header } from '../../components/Header'
+import { CardBalance } from '../../components/CardBalance'
 
 export const Home = () => {
   return (
@@ -13,6 +14,11 @@ export const Home = () => {
       <div className="homeContentWrapper">
         <main className="homeContent">
           <Header />
+          <div className="cardBalanceContainer">
+            <CardBalance label="Entradas" type="deposit" value="R$17.400,00" />
+            <CardBalance label="Saídas" type="checkout" value="R$1.259,00" />
+            <CardBalance label="Total" type="balance" value="R$16.141,00" />
+          </div>
 
           {/* <TransitionTypeCard label="Entrada" transitionType="deposit" /> */}
         </main>
