d4e4ee58eef3d96d48f01d6b55ecc555d5684dc5
wellison E Brito <wellysone.brito@gmail.com>
2023-03-25 17:24:19 -0300

    feat: added elements to footer component

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import './styles.scss'
 
+import { ReactComponent as ArrowLeftIcon } from '../../assets/arrowLeft.svg'
+import { ReactComponent as ArrowRightIcon } from '../../assets/arrowRight.svg'
+
 export const Footer = () => {
-  return <div>teste</div>
+  return (
+    <div className="footerContainer">
+      <span>{<ArrowLeftIcon />}</span>
+      <div className="footerPaginationContainer">
+        <span>1</span>
+        <span className="active">2</span>
+        <span>3</span>
+      </div>
+      <span>{<ArrowRightIcon />}</span>
+    </div>
+  )
 }
